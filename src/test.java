dc881d8d7d4c1e68ccbfd31fd99dac759bae3203
Marco Ascia <63652505+GengisKhanAscia@users.noreply.github.com>
2020-04-15 19:57:04 +0200

    Aggiornamento della classe test.java

--- src/test.java
+++ src/test.java
@@ -1,5 +1,6 @@
 public class test {
     public static void main (String args[]){
-        System.out.println("Questa è una classe test di prova.");
+        System.out.println("Questa è una classe test di prova."); // Non capisco perché mette l'errore qui, quando funziona perfettamente
+        System.out.println("Questa è un update di prova.");
     }
 }
